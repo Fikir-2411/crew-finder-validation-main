92609d377c792e5edaffca5b4a26bd172d9fcdf9
Fikir <fikirmuluken@gmail.com>
2021-08-11 14:50:39 +0300

    user validation body

--- src/validation/user/user.validate.js
+++ src/validation/user/user.validate.js
@@ -2,6 +2,17 @@ import { body,validationResult } from "express-validator";
 
 const validateUser = [
     body('fullName').trim().isLength({min: 3}),
+    body('username'),
+    body('email'),
+    body('password'),
+    body('isActive'),
+    body('role'),
+    body('talent'),
+    body('otherTalents'),
+    body('verification'),
+    body('phoneNumber'),
+    body('resetPasswordToken'),
+    body('resetPasswordExpiration'),
 ];
 
 
